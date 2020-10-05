ed099e56dd376981b50bed9d523eb7efe7da6a6b
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-05 20:27:30 -0300

    finder img  added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './comps/Header'
+import Finder from './comps/Finder'
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Finder />
     </div>
   );
 }
---
+++ src/comps/Finder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import finderimg from '../img/thousand-01.png';
+
+function Finder() {
+  return (
+    <div className="finder">
+      <img className="finder__img" src={finderimg} alt='Holiday fidner img' />
+    </div>
+  )
+}
+
+export default Finder;
