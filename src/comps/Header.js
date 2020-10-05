8bda7f469114511374c979f656c832c9a8315897
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-05 19:20:34 -0300

    header menu items added

--- src/comps/Header.js
+++ src/comps/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
       <div className="logo">
         <div className="logo__orange"></div>
       </div>
+      <ul className="header__menu">
+        <li className="header__menu__item" id="home"><a href="#">Home</a></li>
+        <li className="header__menu__item"><a href="#">Destinations</a></li>
+        <li className="header__menu__item"><a href="#">About</a></li>
+        <li className="header__menu__item"><a href="#">Partner</a></li>
+      </ul>
       
     </div>
   )
