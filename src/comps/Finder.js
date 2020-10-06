ee02cd8c83e5d0472a431a25d65bc0f8745b50cc
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-05 23:31:31 -0300

    finder dropdown added

--- src/comps/Finder.js
+++ src/comps/Finder.js
@@ -8,6 +8,20 @@ function Finder() {
         <h1 className="finder__content__text">Explore and Travel</h1>
         <h4 className="finder__content__smtext">Holiday Finder</h4>
         <hr className="finder__content__line"></hr>
+        <div className="finder__content__dropdown">
+          <select className="finder__content__dropdown__option">
+            <option value="location">Location</option>
+          </select>
+          <select className="finder__content__dropdown__option">
+            <option value="activity">Activity</option>
+          </select>
+          <select className="finder__content__dropdown__option">
+            <option value="grade">Grade</option>
+          </select>
+          <select className="finder__content__dropdown__option">
+            <option value="date">Date</option>
+          </select>
+        </div>
       </div>
       <img className="finder__img" src={finderimg} alt='Holiday finder img' />
     </div>
