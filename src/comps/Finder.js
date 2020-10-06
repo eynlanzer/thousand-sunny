e90d87fd669bf02ad897e37e975a3002f85a70b7
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-05 23:59:17 -0300

    finder btn added

--- src/comps/Finder.js
+++ src/comps/Finder.js
@@ -8,6 +8,8 @@ function Finder() {
         <h1 className="finder__content__text">Explore and Travel</h1>
         <h4 className="finder__content__smtext">Holiday Finder</h4>
         <hr className="finder__content__line"></hr>
+        <br />
+
         <div className="finder__content__dropdown">
           <select className="finder__content__dropdown__option">
             <option value="location">Location</option>
@@ -22,7 +24,10 @@ function Finder() {
             <option value="date">Date</option>
           </select>
         </div>
+        
+        <button className="finder__content__btn">Explore</button>
       </div>
+
       <img className="finder__img" src={finderimg} alt='Holiday finder img' />
     </div>
   )
