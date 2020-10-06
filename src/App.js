4267dcaef375c2b7c97668fa431d74495e6f5783
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-06 11:27:37 -0300

    New way + guides sections added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './comps/Header'
 import Finder from './comps/Finder'
+import NewWay from './comps/NewWay'
+import Guides from './comps/Guides'
 import Footer from './comps/Footer'
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
     <div className="App">
       <Header />
       <Finder />
+      <NewWay />
+      <Guides />
       <Footer />
     </div>
   );
---
+++ src/comps/NewWay.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import newwayimg from '../img/thousand-02.png';
+
+const NewWay = () => {
+  return (
+    <div className="newway">
+      <div className="newway__content">
+        <h1 className="newway__content__title">A new way to explore the world</h1>
+        <p className="newway__content__text">
+          For decades travellers have reached for Lonely Planet books when looking to plan and execute their perfect trip, but now, they can also let Lonely Planet Experiences lead the way
+        </p>
+        <button className="newway__content__button">Learn More</button>      
+      </div>
+
+      <img className="newway__img" src={newwayimg} alt='New way section img' />
+    </div>
+  )
+}
+
+export default NewWay;
