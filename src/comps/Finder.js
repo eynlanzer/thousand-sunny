2d8e21db1e3ae2f46d0f7528d11a95127cd4f429
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-05 21:56:39 -0300

    finder text  added

--- src/comps/Finder.js
+++ src/comps/Finder.js
@@ -4,7 +4,12 @@ import finderimg from '../img/thousand-01.png';
 function Finder() {
   return (
     <div className="finder">
-      <img className="finder__img" src={finderimg} alt='Holiday fidner img' />
+      <div className="finder__content">
+        <h1 className="finder__content__text">Explore and Travel</h1>
+        <h4 className="finder__content__smtext">Holiday Finder</h4>
+        <hr className="finder__content__line"></hr>
+      </div>
+      <img className="finder__img" src={finderimg} alt='Holiday finder img' />
     </div>
   )
 }
