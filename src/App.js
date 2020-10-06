d3c4b337c26b9cbaaa54f8fb9935d41710eff708
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-06 00:18:39 -0300

    footer draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './comps/Header'
 import Finder from './comps/Finder'
+import Footer from './comps/Footer'
 
 const App = () => {
   return (
     <div className="container">
       <Header />
       <Finder />
+      <Footer />
     </div>
   );
 }
---
+++ src/comps/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+      {/* <div className="logo">
+        <div className="logo__orange"></div>
+      </div> */}
+    </div>
+  )
+}
+
+export default Footer;
