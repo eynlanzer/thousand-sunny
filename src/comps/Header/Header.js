c1336eb8f54994572833279428f6379fd9030c28
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-13 15:25:53 -0300

    code cleaning + hamburger icon changed

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -8,18 +8,19 @@ const Header = () => {
   }
 
   return (
-    <div className="header fixed-top">
+    <div className="header fixedTop">
       <div className="logo">
         <div className="logo__center"></div>
       </div>
 
-      <div className="hamburger" onClick={toggler}>
+      <button className="hamburger" onClick={toggler} />
+      {/* <div className="hamburger" onClick={toggler}>
         <svg viewBox="0 0 100 80" width="40" height="40">
           <rect width="100" height="20"></rect>
           <rect y="30" width="100" height="20"></rect>
           <rect y="60" width="100" height="20"></rect>
         </svg>
-      </div>
+      </div> */}
 
       <ul className={isOpen ? "header__menu--open": "header__menu"}>
         <li className="header__menu__item" id="home"><a href="#">Home</a></li>
@@ -27,8 +28,8 @@ const Header = () => {
         <li className="header__menu__item"><a href="#">About</a></li>
         <li className="header__menu__item"><a href="#">Partner</a></li>
         <div className="header__btn">
-          <button className="header__btn__login">Login</button>
-          <button className="header__btn__register">Register</button>
+          <button className="header__btn--login">Login</button>
+          <button className="header__btn--register">Register</button>
         </div>
       </ul>
 
