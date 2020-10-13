f419ce02b980f996c69c48121f7baadb82f0c5cd
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-13 17:22:56 -0300

    fixed burger menu

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -8,29 +8,27 @@ const Header = () => {
   }
 
   return (
-    <div className="header fixedTop">
+    <div className="header">
       <div className="logo">
         <div className="logo__center"></div>
       </div>
 
-      <button className="hamburger" onClick={toggler} />
-      {/* <div className="hamburger" onClick={toggler}>
+      <div className="hamburger" onClick={toggler}>
         <svg viewBox="0 0 100 80" width="40" height="40">
           <rect width="100" height="20"></rect>
           <rect y="30" width="100" height="20"></rect>
           <rect y="60" width="100" height="20"></rect>
         </svg>
-      </div> */}
+      </div>
 
       <ul className={isOpen ? "header__menu--open": "header__menu"}>
+        <button className="header__menu--close" onClick={toggler}>X</button>
         <li className="header__menu__item" id="home"><a href="#">Home</a></li>
         <li className="header__menu__item"><a href="#">Destinations</a></li>
         <li className="header__menu__item"><a href="#">About</a></li>
         <li className="header__menu__item"><a href="#">Partner</a></li>
-        <div className="header__btn">
-          <button className="header__btn--login">Login</button>
-          <button className="header__btn--register">Register</button>
-        </div>
+        <button className="header__btn--login">Login</button>
+        <button className="header__btn--register">Register</button>
       </ul>
 
     </div>
