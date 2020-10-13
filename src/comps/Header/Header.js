49c687cc99e307707fe2e934b6a727a0298e0a93
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-13 17:10:35 -0300

    fixed burger menu

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [loginPage, setLoginPage] = useState(false);
 
   const toggler = () => {
     isOpen ? setIsOpen(false) : setIsOpen(true);
   }
 
   return (
-    <div className="header fixed-top">
+    <div className="header fixedTop">
       <div className="logo">
         <div className="logo__center"></div>
       </div>
@@ -27,8 +28,18 @@ const Header = () => {
         <li className="header__menu__item"><a href="#">About</a></li>
         <li className="header__menu__item"><a href="#">Partner</a></li>
         <div className="header__btn">
-          <button className="header__btn__login">Login</button>
-          <button className="header__btn__register">Register</button>
+          <button onClick={() => setLoginPage(true)} className="header__btn--login">Login</button>
+            <div className={loginPage ? "login--visible" : "login--invisible"} >
+              <div className="login-container">
+                <button onClick={() => setLoginPage(false)} className="login--close">X</button>
+                <h1 className="login-title">Login</h1>
+                <p className="login-greeting">Bom dia! Ficamos felizes em tê-lo conosco.</p>
+                <input className="login-input" type="text" placeHolder="E-mail:"/>
+                <input className="login-input" type="password" placeHolder="Password"/>
+                <input className="login-input" type="submit" value={'Submit'}/>
+              </div>
+            </div>
+          <button className="header__btn--register">Register</button>
         </div>
       </ul>
 
