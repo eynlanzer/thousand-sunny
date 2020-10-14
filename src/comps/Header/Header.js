0d8f2460871c0b7b438184f84685a3c2cd188f5e
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-14 19:13:11 -0300

    login animation added

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  // const [loginPage, setLoginPage] = useState(false);
+  const [loginPage, setLoginPage] = useState(false);
 
   const toggler = () => {
     isOpen ? setIsOpen(false) : setIsOpen(true);
   }
 
-  // const toggleLogin = () => {
-  //   loginPage ? setLoginPage(false) : setLoginPage(true);
-  // }
+  const toggleLogin = () => {
+    loginPage ? setLoginPage(false) : setLoginPage(true);
+  }
 
   return (
     <div className="header">
@@ -32,20 +32,33 @@ const Header = () => {
         <li className="header__menu__item"><a href="#">Destinations</a></li>
         <li className="header__menu__item"><a href="#">About</a></li>
         <li className="header__menu__item"><a href="#">Partner</a></li>
-        <button className="header__btn--login">Login</button>
-        {/* <button className="header__btn--login" onClick={toggleLogin}>Login</button> */}
+        {/* <button className="header__btn--login">Login</button> */}
+        <button className="header__btn--login" onClick={toggleLogin}>Login</button>
         
-        {/* <form className={loginPage ? "loginForm--open" : "loginForm"}>
+        <div className={loginPage ? "loginForm--open" : "loginForm"}>
           <svg onClick={toggleLogin} className="loginForm--close" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0.000151038 14.1426L14.1423 0.000442743L15.5565 1.41466L1.41436 15.5568L0.000151038 14.1426Z" fill="#B4D51E"/>
             <path d="M1.41421 0L15.5563 14.1421L14.1421 15.5563L0 1.41421L1.41421 0Z" fill="#B4D51E"/>
           </svg>
 
-          <h1 className="loginForm__title">Login</h1>
-          <p className="loginForm__greeting">Bom dia! Ficamos felizes em tê-lo conosco.</p>
-          <input className="loginForm__input" type="text" placeHolder="E-mail:"/>
-          <input className="loginForm__input" type="password" placeHolder="Password"/>
-        </form> */}
+          <form className="loginForm__container">
+            <h1 className="loginForm__title">Login</h1>
+            <p className="loginForm__greeting">Bom dia! Ficamos felizes em tê-lo conosco.</p>
+            <input className="loginForm__input" type="text" placeHolder="E-mail:"/>
+            <input className="loginForm__input" type="password" placeHolder="Password:"/>
+            <div className="loginForm__actions">
+            <a href='#'>
+              <p className="loginForm__actions__link">Quero uma conta ></p>
+            </a>
+              <button className="loginForm__actions__btn">
+                Entrar
+                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 13L7 7L0.999999 1" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+              </button>
+            </div>
+          </form>
+        </div>
 
         <button className="header__btn--register">Register</button>
       </ul>
