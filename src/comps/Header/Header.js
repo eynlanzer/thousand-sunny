8d73d423b036773f538fb986e6c4ea2ab8d5d0e0
Manoel do Nascimento <manu3105@yahoo.com>
2020-10-16 17:39:29 -0300

    standard login menu + fixed burger/login close

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
 
   const toggleLogin = () => {
     loginPage ? setLoginPage(false) : setLoginPage(true);
+    setIsOpen(false);
   }
 
   return (
@@ -37,14 +38,18 @@ const Header = () => {
         
         <div className={loginPage ? "loginForm--open" : "loginForm"}>
           <form className={loginPage ? "loginForm__container--visible" : "loginForm__container"}>
-            <svg onClick={toggleLogin} className="loginForm--close" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* <svg onClick={toggleLogin} className="loginForm--close" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M0.000151038 14.1426L14.1423 0.000442743L15.5565 1.41466L1.41436 15.5568L0.000151038 14.1426Z" fill="#B4D51E"/>
               <path d="M1.41421 0L15.5563 14.1421L14.1421 15.5563L0 1.41421L1.41421 0Z" fill="#B4D51E"/>
+            </svg> */}
+            <svg onClick={toggleLogin} className="loginForm--close" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#B4D51E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
             </svg>
 
             <h1 className="loginForm__title">Login</h1>
-            <p className="loginForm__greeting">Bom dia! Ficamos felizes em tê-lo conosco.</p>
-            <input className="loginForm__input" type="text" placeHolder="E-mail"/>
+            <p className="loginForm__greeting">Boa tarde! Ficamos felizes em tê-lo conosco.</p>
+            <input className="loginForm__input" type="text" placeHolder="Email"/>
             <input className="loginForm__input" type="password" placeHolder="Senha"/>
             <div className="loginForm__actions">
               <a href='#'>
@@ -54,9 +59,12 @@ const Header = () => {
               </a>
               <button className="loginForm__actions__btn">
                 Entrar
-                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 13L7 7L0.999999 1" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <polyline points="9 18 15 12 9 6"></polyline>
                 </svg>
+                {/* <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1 13L7 7L0.999999 1" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg> */}
               </button>
             </div>
           </form>
